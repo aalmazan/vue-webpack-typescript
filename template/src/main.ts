34ac35aa09f2367ac559521ada982a0f1854bbd0
levym <ducksoupdev@gmail.com>
2018-01-08 13:50:15 +0000

    Make hot reloading optional

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,13 +1,13 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import { makeHot, reload } from './util/hot-reload';
+import VueRouter from 'vue-router';{{#hotReload}}
+import { makeHot, reload } from './util/hot-reload';{{/hotReload}}
 import { createRouter } from './router';
 
 const navbarComponent = () => import('./components/navbar').then(({ NavbarComponent }) => NavbarComponent);
 // const navbarComponent = () => import(/* webpackChunkName: 'navbar' */'./components/navbar').then(({ NavbarComponent }) => NavbarComponent);
 
 import './sass/main.scss';
-
+{{#hotReload}}
 if (process.env.ENV === 'development' && module.hot) {
   const navbarModuleId = './components/navbar';
 
@@ -15,7 +15,7 @@ if (process.env.ENV === 'development' && module.hot) {
   // see https://github.com/webpack/webpack/issues/5668
   makeHot(navbarModuleId, navbarComponent,
     module.hot.accept('./components/navbar', () => reload(navbarModuleId, (<any>require('./components/navbar')).NavbarComponent)));
-}
+}{{/hotReload}}
 
 new Vue({
   el: '#app-main',
--- template/src/router.ts
+++ template/src/router.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
-import VueRouter, { Location, Route, RouteConfig } from 'vue-router';
-import { makeHot, reload } from './util/hot-reload';
+import VueRouter, { Location, Route, RouteConfig } from 'vue-router';{{#hotReload}}
+import { makeHot, reload } from './util/hot-reload';{{/hotReload}}
 
 const homeComponent = () => import('./components/home').then(({ HomeComponent }) => HomeComponent);
 const aboutComponent = () => import('./components/about').then(({ AboutComponent }) => AboutComponent);
@@ -8,7 +8,7 @@ const listComponent = () => import('./components/list').then(({ ListComponent })
 // const homeComponent = () => import(/* webpackChunkName: 'home' */'./components/home').then(({ HomeComponent }) => HomeComponent);
 // const aboutComponent = () => import(/* webpackChunkName: 'about' */'./components/about').then(({ AboutComponent }) => AboutComponent);
 // const listComponent = () => import(/* webpackChunkName: 'list' */'./components/list').then(({ ListComponent }) => ListComponent);
-
+{{#hotReload}}
 if (process.env.ENV === 'development' && module.hot) {
   const homeModuleId = './components/home';
   const aboutModuleId = './components/about';
@@ -24,7 +24,7 @@ if (process.env.ENV === 'development' && module.hot) {
 
   makeHot(listModuleId, listComponent,
     module.hot.accept('./components/list', () => reload(listModuleId, (<any>require('./components/list')).ListComponent)));
-}
+}{{/hotReload}}
 
 Vue.use(VueRouter);
 
